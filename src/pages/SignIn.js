b072caedba77546bf5be2c0b1330b9c1188d8b9e
Oguzhan Kayapinar <oguzhankayapinar@gmail.com>
2023-03-05 22:01:27 +0300

    V1.3.1_SignIn-SignUp

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { TextField, CssBaseline, Button, Box, Container, Link, Avatar, Typography } from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 
 function SignIn() {
     return (
-        <h1>SignIn</h1>
+        <>
+            <CssBaseline />
+            <Container maxWidth="xs">
+                <Box component="form" sx={{ mt: 8 }} >
+                    <Avatar sx={{ mx: "auto", bgcolor: "primary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography variant='h4' sx={{ textAlign: "center" }}> Sign In </Typography>
+                    <TextField
+                        margin='normal'
+                        fullWidth
+                        required
+                        autoComplete='email'
+                        label="Email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin='normal'
+                        fullWidth
+                        required
+                        label="Password"
+                        type='password'
+                    />
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        fullWidth
+                        sx={{ mt: 2 }}
+                    >Sign in
+                    </Button>
+                    <Box sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        flexDirection: "column",
+                        mt: 1
+                    }}>
+                        <Link>Forgot Password</Link>
+
+                        <Link>Don't have an account ? Sign Up </Link>
+                    </Box>
+                </Box>
+            </Container>
+        </>
     )
 }
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React from 'react'
+import { TextField, CssBaseline, Button, Box, Container, Link, Avatar, Typography } from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 
-const SignUp = () => {
+function SignUp() {
     return (
-        <h1>
-            SignUp
-        </h1>
-    )
-};
+        <>
+            <CssBaseline />
+            <Container maxWidth="xs">
+                <Box component="form" sx={{ mt: 8 }} >
+                    <Avatar sx={{ mx: "auto", bgcolor: "primary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography variant='h4' sx={{ textAlign: "center" }}> Sign Up </Typography>
 
-export default SignUp;
+                    <TextField
+                        fullWidth
+                        margin='normal'
+                        label="Full Name"
+                        required
+                        autoComplete='name'
+                        autoFocus
+                    />
+
+                    <TextField
+                        margin='normal'
+                        fullWidth
+                        required
+                        autoComplete='email'
+                        label="Email"
+
+                    />
+
+                    <TextField
+                        margin='normal'
+                        fullWidth
+                        required
+                        label="Password"
+                        type='password'
+                    />
+
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        fullWidth
+                        sx={{ mt: 2 }}
+                    >Sign Up
+                    </Button>
+
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        mt: 2
+                    }}>
+                        <Link>Already an account? Sign In </Link>
+                    </Box>
+
+                </Box>
+            </Container>
+        </>
+    )
+}
+
+export default SignUp
